06592a6109796a3cc7fb57f74b0ea4b2535da093
joaoleao123 <joaoleaosibs@gmail.com>
2026-03-27 19:00:36 +0000

    teste

--- src/main/webapp/download_demo/js/download_demo_js.js
+++ src/main/webapp/download_demo/js/download_demo_js.js
@@ -2,5 +2,5 @@ window.addEventListener("DOMContentLoaded", () => {
   const basePath = window.location.pathname.split('/')[1];
   const prefix = basePath === 'SimuladorSIBS' ? '/SimuladorSIBS' : '';
 
-  document.getElementById('downloadBtn').href = `/download`;
+  document.getElementById('downloadBtn').href = `${prefix}/download`;
 });
